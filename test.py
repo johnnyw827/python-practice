890923f497a5131cd661846435bdd97dda37d6a1
Johnny <johnnyw827@gmail.com>
2020-11-22 10:02:11 -0500

    add variables practice

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-print ("test")
+
+#print statement
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+#variables
+character_name = "Bob"
+character_age = "25"
+
+print(character_name)
+print(character_age)
+#changing variables, data types
+character_age = 10.5
+isMale = True
+
+phrase = "This Is A Sentence"
+print("learn \n\"now\" " + phrase)
+
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.lower().isupper())
+print(len(phrase))
