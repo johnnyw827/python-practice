360d1bbb9674b119557507c817e82089cd8d47c5
Johnny <jrw9@uscupstate.edu>
2020-12-09 14:36:58 -0500

    More Files

---
+++ lists.py
@@ -0,0 +1,11 @@
+friends = ["Kevin", "john", "jim", "bob", "sue"]
+numbers = [3, 5, 2, 6, 1]
+friends.extend(numbers)
+friends.insert(1, "Kelly")
+
+print(friends)
+
+
+friends.clear()
+
+print(friends)
---
+++ madlibs.py
@@ -0,0 +1,8 @@
+
+color = input("Enter a color.")
+pluralnoun = input("Enter a plural noun.")
+celebrity = input("Enter a celebrity.")
+
+print("Roses are " + color)
+print(pluralnoun + " are blue")
+print("I love " + celebrity)
